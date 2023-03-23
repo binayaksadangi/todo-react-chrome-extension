add32a3211a45f25c1b73ac9582f96163c1af9af
IncBinayak <binayak.sadangi@incture.com>
2023-03-23 16:24:46 +0530

    version-1

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const getTodos = () => {
+  if (localStorage.getItem("todos") === null) {
+    return [];
+  } else {
+    let todoLocal = JSON.parse(localStorage.getItem("todos"));
+    return todoLocal;
+  }
+};
+
+function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(getTodos());
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  const deleteTodo = (ind) => {
+    setTodos(todos.filter((todo, i) => i != ind));
+  };
+  return (
+    <div className="container">
+      <h1 className="heading">Todo List</h1>
+      <div className="input-container">
+        <input
+        className="input"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+        className="btn"
+          onClick={() => {
+            setTodos([...todos, input]);
+            setInput("");
+          }}
+        >
+         
+          +
+        </button>
+        </div>
+        <ul className="list-container">
+          {todos.map((todo, index) => (
+            <li className="list-item" key={index}>
+              {todo} <button className="delete-btn" onClick={() => deleteTodo(index)}>X</button>
+            </li>
+          ))}
+        </ul>
+    </div>
+  );
+}
+
+export default App;
